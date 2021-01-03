2c0044f924aa63230bb8d95ab88fc522b364dff0
vishnuteja <vishnuteja42@outlook.com>
2021-01-03 19:56:02 +0530

    first commit

---
+++ hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a;      // variable declaration..
+    int b=20;   
+    a=10;   // variable assignment
+    int c=a+b;   // adding two numbers
+    printf("Hello to C language...!");
+    printf("\aThe sum of two numbers is %d",c); 
+    return 0;
+}
