261825417c47a956121b0a105374f684ed551a84
vishnuteja <vishnuteja42@outlook.com>
2021-01-06 20:09:15 +0530

    6/01/2021

---
+++ first_program.java
@@ -0,0 +1,17 @@
+// We call function as methods here...
+// keywords..
+
+// int, float, double byte short long [number data types]
+// char, String 
+
+class Add2                 // class definition..
+{
+    public static void main(String args[]) {
+        int first_number = 10;
+        int second_number = 20;
+        int sum = first_number+second_number;
+        System.out.println("Hello welcome to JAVA language...");
+        System.out.println("This is first class");
+        System.out.println("the sum of "+first_number+" and "+second_number+" is "+sum);
+    }
+}
--- hello_world.c
+++ hello_world.c
@@ -1,11 +1,13 @@
+//  compiler : converts high level languates to computer readable languages..
+
 #include <stdio.h>
 int main()
-{
+{               // method body
     int a;      // variable declaration..
     int b=20;   
     a=10;   // variable assignment
     int c=a+b;   // adding two numbers
-    printf("Hello to C language...!");
+    printf("Hello to C language...!");    //method call
     printf("\aThe sum of two numbers is %d",c); 
     return 0;
 }
