fbdcd010c48f595670cce6742a7e51675f5bc51a
vishnuteja <vishnuteja42@outlook.com>
2021-01-08 20:50:50 +0530

    For loop

---
+++ Looping.java
@@ -0,0 +1,17 @@
+class Looping
+{
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int sum = 0;
+        //initialisatin
+        int i = 0;
+        while(i<=n)
+        {
+            //updation.
+        }
+        // for(int i=1;i<=n;i=i+1) // 10<10
+        // {
+        //     System.out.println("Repeat this n times..."+i);
+        // }
+    }
+}
