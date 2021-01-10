ecd3dc027ff87e9fa9dd320a86bdb3654370775a
vishnuteja <vishnuteja42@outlook.com>
2021-01-10 21:20:26 +0530

    looping

--- Looping.java
+++ Looping.java
@@ -2,16 +2,17 @@ class Looping
 {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        int sum = 0;
+        // int sum = 0;
         //initialisatin
-        int i = 0;
-        while(i<=n)
-        {
-            //updation.
-        }
-        // for(int i=1;i<=n;i=i+1) // 10<10
+        // int i = 0;
+        // while(i<=n)
         // {
-        //     System.out.println("Repeat this n times..."+i);
+        //     //updation.
         // }
+        int i;
+        for(i=1;i<=n;i=i+1) // 10<10
+        {
+            System.out.println("Repeat this n times..."+i);
+        }
     }
 }
