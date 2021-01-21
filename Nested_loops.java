90de778f5ad61d5f73e8db2e34a94f06f78626a3
vishnuteja <vishnuteja42@outlook.com>
2021-01-21 21:27:18 +0530

    nested for loop

--- Nested_loops.java
+++ Nested_loops.java
@@ -3,6 +3,7 @@ class Nested
     public static void main(String args[])
     {
         int n  = Integer.parseInt(args[0]);
+        // int interval= Integer.parseInt((args[1]))
         for(int j=1;j<=n;j++)
         {
             for(int i = 1;i<=n;i++)
