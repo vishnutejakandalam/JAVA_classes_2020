0ce5d417797dc4c5245e7433c46cab7485e66031
vishnuteja <vishnuteja42@outlook.com>
2021-01-23 08:04:57 +0530

    Break and continue

--- Nested_loops.java
+++ Nested_loops.java
@@ -2,13 +2,18 @@ class Nested
 {
     public static void main(String args[])
     {
-        int n  = Integer.parseInt(args[0]);
+        int n  = Integer.parseInt(args[0]);  // n= 5
         // int interval= Integer.parseInt((args[1]))
-        for(int j=1;j<=n;j++)
+
+        for(int i=1;i<=n;i++)   // i = 6, 
         {
-            for(int i = 1;i<=n;i++)
+            for(int j = 1;j<=n;j++)   // j = 5
             {       
-                System.out.print("*");
+                // if(j == 13)
+                //     continue;
+                System.out.print(" "+j);  
+                if(i==j)
+                    break;
             }        
             System.out.println();
         }
