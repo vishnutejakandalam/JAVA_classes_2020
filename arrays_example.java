ea6a7df67d3a56ebd3693e344e41f975ec7dbdcd
vishnuteja <vishnuteja42@outlook.com>
2021-01-26 07:26:50 +0530

    arrays_example

---
+++ arrays_example.java
@@ -0,0 +1,15 @@
+import javax.swing.plaf.synth.SynthMenuBarUI;
+
+public class arrays_example {
+    public static void main(String[] args) {
+        int[] ar = {90,2,3,26,-5};
+         System.out.println(ar[4]); // toa access an element in the array
+         System.out.println(ar.length); // to get size of an array
+         ar[2] = 24;
+         System.out.println(ar[2]);
+         for(int i=0;i<ar.length;i++)
+            ar[i] = i;
+        for(int i=0;i<ar.length;i++)
+            System.out.println("An element at position "+i+"in array is "+ar[i]);
+        }
+}
