e6b2bddd7b3f11366395334be98477106bc9ab9f
vishnuteja <vishnuteja42@outlook.com>
2021-02-25 08:05:13 +0530

    added topics

--- demopack/Basic.java
+++ demopack/Basic.java
@@ -1,11 +1,12 @@
+package demopack;
 class Basic {
     private int coins;
     void update_coins()
     {
         coins = coins + 1;
-        System.out.println("Coins updated to "+coins);
+        display();
     }
-    void display()
+    private void display()
     {
         System.out.println("THe value is "+coins);
     }
@@ -20,11 +21,8 @@ class Basic_main
 {
     public static void main(String[] args) {
         Basic obj = new Basic();
-        obj.coins = 20;
         obj.update_coins();
         obj.display();
-        Extended obj1 = new Extended();
         // obj1.coins = 310;
-        obj1.show();
     }
 }
