2507ca9e733c26a4c2592ed23db1c8103142499e
vishnuteja <vishnuteja42@outlook.com>
2021-03-02 08:22:41 +0530

    method overriding

---
+++ Instabook/Instabook.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class Instabook
+{
+    public static void main(String[] args) {
+    String[] usernames = {"coffe","juice","banana"};
+    String[] passwords = {"tea", "mango","apple"};
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter username to login");
+    String user = sc.nextLine();
+
+    System.out.println("Enter password to login");
+    String pass = sc.nextLine();
+    }
+}
